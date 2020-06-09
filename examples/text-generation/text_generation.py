82b5dc3d18893f3cc60627e93f7134b027859043
northfoxz <northfoxz@northfoxzde-MacBook-Air.local>
2020-06-09 23:32:53 +0800

    fix variable bug

--- examples/text-generation/text_generation.py
+++ examples/text-generation/text_generation.py
@@ -63,7 +63,7 @@ def main():
 
 		# Add the prompt at the beginning of the sequence. Remove the excess text that was used for pre-processing
 		total_sequence = (
-			prompt_text + text[len(tokenizer.decode(encoded_prompt[0], clean_up_tokenization_spaces=True)) :]
+			PROMPT + text[len(tokenizer.decode(encoded_prompt[0], clean_up_tokenization_spaces=True)) :]
 		)
 
 		generated_sequences.append(total_sequence)
